662ffb677e8086ce6c398bf113e475c48625b19b
beautifulnature <heimarunag@gmail.com>
2025-06-16 07:33:43 +0530

    udemy python developer complete course

---
+++ python/udemy/Python_Developer_Complete_course_2024-12/main.py
@@ -0,0 +1,18 @@
+# print("hello")
+# print()
+# print("my name is foo")
+# print("")
+# print("yes, foo is my name")
+# print("yes, it's my name")
+# print('yes, it"s my name')
+
+# name = "foo"
+# print('my name is', name)
+# print('my name is ' + "foo")
+# print('my name is ' +  name)
+# print(f'my name is {name}')
+
+name = input("what is your name? ")
+age = input("what is your age? ")
+# print('hi {name} ' + 'you are ' + age + ' years old')
+print(f'hi {name}, you are {age} years old')
